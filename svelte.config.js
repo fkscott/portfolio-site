d94f01b34bc2bae9056365dc18c65dab317ed2a4
Keagan <fkscott@loyola.edu>
2022-03-15 11:23:11 -0400

    update config for vercel deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,15 @@
-import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
- // Consult https://github.com/sveltejs/svelte-preprocess
- // for more information about preprocessors
- preprocess: preprocess(),
+	kit: {
+		adapter: adapter(),
 
- kit: {
- // hydrate the <div id="svelte"> element in src/app.html
-  adapter: adapter({
-    pages: 'build',
-    assets: 'build',
-    fallback: null
-  })
- }
+		// Override http methods in the Todo forms
+		methodOverride: {
+			allowed: ['PATCH', 'DELETE']
+		}
+	}
 };
 
 export default config;
